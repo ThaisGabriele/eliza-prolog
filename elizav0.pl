33b227c09578bd1c78bf10870d6599aa9b407ebf
ThaisGabriele <tgcn@icomp.ufam.edu.br>
2021-11-28 20:37:20 -0400

    aceitando mais algumas expressoes

--- elizav0.pl
+++ elizav0.pl
@@ -6,6 +6,7 @@
 %% [Inicializacao] - inicializa o script atraves da funcao main
 %%:- initialization(main).
 
+:- dynamic(base_respostas/2).
 %% [DCG]
 
 oi --> [oi].
@@ -14,9 +15,16 @@ nome --> [nome].
 desculpe --> [desculpe].
 %% meupai --> [meu, pai].
 sair --> [bye].
+sonho --> [sonho].
+minha --> [minha].
 
+%% Base de respostas
+
+%% base_respostas(_,_).
 
 %% Regras
+
+%% nova_resp(P,S) :- [P,S].
 
 sublist([P|R], L) :-
     append([_, [P|R], _], L).
@@ -29,6 +37,7 @@ frase(meupai, L) --> [meu, pai], palavras(L).
 frase(meupai, L) --> palavras(_), [meu, pai], palavras(L).
 
 frase(sonhei_com, L) --> [eu, sonhei, com], palavras(L).
+frase(sonhei_com, L) --> palavras(_), [eu, sonhei, com], pronome(minha,L), palavras(L).
 frase(sonhei_com, L) --> palavras(_), [eu, sonhei, com], palavras(L).
 
 frase(estou_feliz, L) --> [estou, feliz], palavras(L).
@@ -37,8 +46,29 @@ frase(estou_feliz, L) --> palavras(_), [estou, feliz], palavras(L).
 frase(estou_triste, L) --> [estou, triste], palavras(L).
 frase(estou_triste, L) --> palavras(_), [estou, triste], palavras(L).
 
+frase(estou_triste, L) --> [estou, triste], palavras(L).
+frase(estou_triste, L) --> palavras(_), [estou, triste], palavras(L).
+
 palavras([]) --> [].
 palavras([P|R]) --> [P], palavras(R).
+
+nova_resp_sonhei_com(sonhei_com, L, P, S) --> palavras(_), [eu, sonhei, com], pronome(minha, P), subst(palavra,S), palavras(L).
+
+%% nova_resp_sonhei_com(sonhei_com, L, P, S) :- 
+%%    palavras(_), [eu, sonhei, com], pronome(minha, P), subst(palavra,S), palavras(L),
+    
+
+/*
+pega_curinga2(E, newList) :-
+    insert()
+*/
+subst(palavra, S) --> [S].
+
+pronome(minha, L) --> [sua].
+
+% member(Item, List) - succeeds if the item is a member of the list;
+%   List must be instantiated at the time member is called. Item need not
+%   be instantiated.
 
 %% Interpretador de linguagem natural
 %% Nesta parte, o objetivo é interpretar a entrada do usuário e descobrir com qual regra ela se adequa
@@ -46,13 +76,45 @@ palavras([P|R]) --> [P], palavras(R).
 interpretar(E,eh_computador) :-sublist(computador,E).
 
 interpretar(E,meu_pai) :- 
-    frase(meupai, L, E, []).
+    frase(meupai, _, E, []).
+
+/*
+interpretar(E,tem_sonhei_com) :- 
+    frase(sonhei_com, L, E, []),
+    nova_resp_sonhei_com(L, nova_resp).
+
+*/
+/* sonhei com -
+ Entrada  E = [eu, sonhei, com, minha, viagem]
+ Como você se sente em relação a [2] na verdade?
+ Como você se sente em relação a [sua viagem] na verdade?
+*/
+
+% Examples of use:
+% ?- member(b, [a, b, c]).
+% true.
+% 
+% ?- member(X, [a, b, c]).
+% X = a ;
+% X = b ;
+% X = c ;
+% false.
+
+
+/*
+interpretar(E,tem_sonhei_com) :- 
+    frase(sonhei_com, L, E, []).
+*/
+
+interpretar(E,tem_sonhei_com) :- 
+    frase(sonhei_com, L, E, []),
+    asserta(base_respostas(L,sonhei_com)).
 
 interpretar(E,tem_estou_feliz) :- 
-    frase(estou_feliz, L, E, []).
+    frase(estou_feliz, _, E, []).
 
 interpretar(E,tem_estou_triste) :- 
-    frase(estou_triste, L, E, []).
+    frase(estou_triste, _, E, []).
 
 interpretar(E,eh_oi) :- sublist(oi,E).
 
@@ -60,11 +122,22 @@ interpretar(E,tem_nome) :- sublist(nome,E).
 
 interpretar(E,tem_desculpe):- sublist(desculpe,E).
 
+interpretar(E,tem_sonho) :- sublist(sonho,E). 
+
+interpretar(E,tem_parece) :- sublist(parece,E). 
+
+interpretar(E,tem_mesmo) :- sublist(mesmo,E). 
+
+interpretar(E,tem_sou) :- sublist(sou,E). 
+
 interpretar(E,frase_sair) :- sair(E,[]). 
 
 interpretar(_,frase_nao_reconhecida).
 
 %% teste
+
+write_name((_, Name)) :-
+  writeln(Name).
 
 responder(meu_pai) :-
     random_member(Resp, ['Seu pai?',
@@ -85,6 +158,15 @@ responder(tem_estou_triste) :-
                         'Estou certo de que não é prazeroso estar assim']),
     enumere(Resp),
     interagir.    
+
+responder(tem_sonhei_com):-
+  %%   asserta(base_respostas(P,S)),
+    call(base_respostas,Resp,S),
+   %% enumere(L),
+   %% Res = term_string(S),
+    write("Como você se sente em relação a "), write(Resp), write(" na verdade?"), nl,nl,
+    interagir.
+
 
 responder(eh_oi) :-
     write("Como vai você? Por favor, me fale do seu problema."),nl,nl,
@@ -109,9 +191,39 @@ responder(tem_desculpe) :-
     enumere(Resp),
     interagir.
 
+responder(tem_sonho) :-
+    random_member(Resp, ['O que este sonho sugere a você?',
+                        'Você sonha com frequencia?',
+                        'Que pessoas aparecem em seus sonhos?',
+                        'Você não acha que sonhos tem algo a ver com o seu problema?']),
+    enumere(Resp),
+    interagir.
+
+responder(tem_parece) :-
+    random_member(Resp, ['De que forma?',
+                'Que similaridades há?']),
+    enumere(Resp),
+    interagir.
+
+responder(tem_mesmo) :-
+    write('Que outras conexões você observa?'),
+    interagir.
+
+responder(tem_sou) :-
+    random_member(Resp, ['Porque você está dizendo "SOU"?',
+                'Nao entendi']),
+    enumere(Resp),
+    interagir.
 
 responder(frase_nao_reconhecida) :-
-    write("Não entendi :/ "), nl, nl,
+    random_member(Resp, ['Muito interessante.',
+                        'Não sei se entendi você direito',
+                        'O que isso sugere a você?',
+                        'Por favor, continue.',
+                        'Continue',
+                        'Você quer mesmo falar sobre isso?',
+                        'Elabore melhor']),
+    enumere(Resp),
     interagir.
 
 responder(frase_sair) :-
@@ -140,7 +252,6 @@ prompt(L) :-
     atom_codes(A, NCs), %% A ~atomos aka palavras
     string_lower(A, AP),
     atomic_list_concat(L, ' ', AP). %% aqui vira uma lista
-    %% delete(LP, '', L). %% para remover os brancos
 
 %% Laço de interação com o usuário 
 
